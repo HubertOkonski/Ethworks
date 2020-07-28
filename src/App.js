c3fdf788bc5827b35dcc09a9dd4bac06bb47d75e
Hubert Okonski <hubertokonski1@gmail.com>
2020-07-28 21:39:37 +0200

    task completed

--- src/App.js
+++ src/App.js
@@ -1,122 +1,153 @@
-import React from "react";
-import "./App.css";
-function App() {
-  const splitter = (expression) => {
-    return expression.split(" ");
-  };
-  const emptyValueCleaner = (array) => {
-    let cpyArray = [];
-    array.forEach((value, index) => {
-      if (value !== "") {
-        cpyArray.push(value);
-      }
-    });
-    return cpyArray;
-  };
-  const signDetector = (addendArray) => {
-    let arrayWithSignsInside = [];
-    addendArray.forEach((exp, index, array) => {
-      if (array[index - 1] === "-") arrayWithSignsInside.push("-" + exp);
-      else if (array[index - 1] === "+" || array[index - 1] === undefined)
-        arrayWithSignsInside.push(exp);
-    });
-    return arrayWithSignsInside;
-  };
-  const fractionStrToDecimal = (str) => str.split("/").reduce((p, c) => p / c);
+let augend = " - 10x2 + 6x - 0.75";
+let addend = " 3/2x + 12x0";
+const splitter = (expression) => {
+  return expression.split(" ");
+};
+const emptyValueCleaner = (array) => {
+  // CLEANING VALUE IN CASE OF UNWANTED SPACES
+  let cpyArray = [];
+  array.forEach((value, index) => {
+    if (value !== "") {
+      cpyArray.push(value);
+    }
+  });
+  return cpyArray;
+};
+const signDetector = (addendArray) => {
+  //  DETECTING SIGNS AND SAVING THEM TO ARRAY
+  let arrayWithSignsInside = [];
+  addendArray.forEach((exp, index, array) => {
+    if (array[index - 1] === "-") arrayWithSignsInside.push("-" + exp);
+    else if (
+      array[index - 1] === "+" ||
+      (array[index - 1] === undefined && exp !== "-")
+    ) {
+      arrayWithSignsInside.push(exp);
+    }
+  });
+  return arrayWithSignsInside;
+};
+const fractionStrToDecimal = (str) => str.split("/").reduce((p, c) => p / c);
 
-  const coefficientFixer = (expArray) => {
-    expArray = expArray.map((value) => {
-      if (value.includes("/")) value = fractionStrToDecimal(value);
-      value = parseFloat(value);
-      if (isNaN(value)) {
-        value = 1;
+const coefficientFixer = (expArray) => {
+  // FIXING EXPONENT IN CASE THAT WASNT WRITTEN EXAMPLE: "x" INSTEAD OF "x1"
+  expArray = expArray.map((value) => {
+    if (value.includes("/")) value = fractionStrToDecimal(value);
+    if (value === "-") value = -1;
+    value = parseFloat(value);
+    if (isNaN(value)) {
+      value = 1;
+    }
+    return value;
+  });
+  return expArray;
+};
+const coefficientGetter = (addendArray) => {
+  // GETTING COEFFICIENTS FROM STRING AND PUSHING IT TO AN ARRAY
+  let coefficientArray = [];
+  addendArray.forEach((exp) => {
+    let expArray = exp.split("x");
+    expArray = coefficientFixer(expArray);
+    if (expArray[1] === undefined) expArray[1] = 0;
+    coefficientArray.push({
+      exp: expArray[1],
+      coef: expArray[0],
+    });
+  });
+  return coefficientArray;
+};
+const fillWithMissingAugendCoefArr = (augendCoefArr, result) => {
+  // ADDING COEFFICIENTS THAT WEREN'T INCLUDED IN addingCoef FUNCTION
+  augendCoefArr.forEach((algExp) => {
+    let finded = false;
+    result.forEach((algExp2, index, array) => {
+      if (algExp.exp === algExp2.exp) {
+        finded = true;
+      } else if (!finded && index + 1 === array.length) result.push(algExp);
+    });
+  });
+};
+const adddingCoef = (augendCoefArr, addendCoefArr) => {
+  // ADDING COEFFICIENTS WITH THE SAME EXPONENT
+  let result = [];
+  addendCoefArr.forEach((algExp) => {
+    let finded = false;
+    augendCoefArr.forEach((algExp2, index, array) => {
+      if (algExp.exp === algExp2.exp) {
+        finded = true;
+        result.push({
+          exp: algExp.exp,
+          coef: algExp.coef + algExp2.coef,
+        });
+      } else if (index === array.length - 1 && !finded) result.push(algExp);
+    });
+  });
+  fillWithMissingAugendCoefArr(augendCoefArr, result);
+  return result;
+};
+const sortResultArray = (result) => {
+  result.sort(function (a, b) {
+    return b.exp - a.exp;
+  });
+  return result;
+};
+const printResult = (result) => {
+  let resultString = "";
+  result.forEach((algExp) => {
+    if (algExp.coef > 0 && resultString !== "")
+      resultString = resultString + " + ";
+    else if (algExp.coef < 0) resultString = resultString + " - ";
+    if (algExp.exp !== 0)
+      resultString = resultString + `${Math.abs(algExp.coef)}x${algExp.exp}`;
+    else resultString = resultString + Math.abs(algExp.coef);
+  });
+  return resultString;
+};
+const removeEmptyAlgExp = (arrayOfCoeffs) => {
+  // REMOVING VALUE FROM ARRAY IN CASE COEFFICIENT IS EQUAL TO ZERO
+  arrayOfCoeffs.forEach((algExp, index) => {
+    if (algExp.coef === 0) arrayOfCoeffs.splice(index, 1);
+  });
+  return arrayOfCoeffs;
+};
+const expressionSimplifier = (arrayOfCoeffs) => {
+  // SIMPLIFYING SINGLE EXPRESSION EXAMPLE : "2 + 5 + 7 + x" => "7 + x"
+  let auxiliaryArr = arrayOfCoeffs.map((obj) => ({ ...obj }));
+  auxiliaryArr.forEach((algExp, index, array) => {
+    for (let i = index; i < array.length; i++) {
+      if (i !== index && algExp.exp === array[i].exp) {
+        algExp.coef = algExp.coef + array[i].coef;
+        array.splice(i, 1);
+        i--;
       }
-      return value;
-    });
-    return expArray;
-  };
-  const coefficientGetter = (addendArray) => {
-    let coefficientArray = [];
-    addendArray.forEach((exp) => {
-      let expArray = exp.split("x");
-      expArray = coefficientFixer(expArray);
-      if (expArray[1] === undefined) expArray[1] = 0;
-      coefficientArray.push({
-        exp: expArray[1],
-        coef: expArray[0],
-      });
-    });
-    return coefficientArray;
-  };
-  const fillWithMissingAugendCoefArr = (augendCoefArr, result) => {
-    augendCoefArr.forEach((algExp) => {
-      let finded = false;
-      result.forEach((algExp2, index, array) => {
-        if (algExp.exp === algExp2.exp) {
-          finded = true;
-        } else if (!finded && index + 1 === array.length) result.push(algExp);
-      });
-    });
-    console.log(result);
-  };
-  const adddingCoef = (augendCoefArr, addendCoefArr) => {
-    let result = [];
-    addendCoefArr.forEach((algExp) => {
-      let finded = false;
-      augendCoefArr.forEach((algExp2, index, array) => {
-        if (algExp.exp === algExp2.exp) {
-          finded = true;
-          result.push({
-            exp: algExp.exp,
-            coef: algExp.coef + algExp2.coef,
-          });
-        } else if (index === array.length - 1 && !finded) result.push(algExp);
-      });
-    });
-    fillWithMissingAugendCoefArr(augendCoefArr, result);
-    return result;
-  };
-  const sortResultArray = (result) => {
-    result.sort(function (a, b) {
-      return b.exp - a.exp;
-    });
-    return result;
-  };
-  const printResult = (result) => {
-    let resultString = "";
-    result.forEach((algExp) => {
-      if (algExp.coef > 0 && resultString !== "")
-        resultString = resultString + " + ";
-      else if (algExp.coef < 0) resultString = resultString + " - ";
-      if (algExp.exp !== 0)
-        resultString = resultString + `${Math.abs(algExp.coef)}x${algExp.exp}`;
-      else resultString = resultString + Math.abs(algExp.coef);
-    });
-    console.log(resultString);
-  };
-  const addingExpressions = (augend, addend) => {
-    let coefficientsAddend;
-    let coefficientsAugend;
-    let result;
-    let augendArray = splitter(augend);
-    augendArray = emptyValueCleaner(augendArray);
-    augendArray = signDetector(augendArray);
-    coefficientsAugend = coefficientGetter(augendArray);
-    let addendArray = splitter(addend);
-    addendArray = emptyValueCleaner(addendArray);
-    addendArray = signDetector(addendArray);
-    coefficientsAddend = coefficientGetter(addendArray);
-    result = adddingCoef(coefficientsAugend, coefficientsAddend);
-    result = sortResultArray(result);
-    console.log(coefficientsAddend);
-    console.log(coefficientsAugend);
-    console.log(result);
-    printResult(result);
-  };
-  addingExpressions("3x1/4 + 2x3 + 10x - 4", "12x1/4 + 7x3 + 5x - 12");
-  /// 15x4 + 9x3 + 15x - 16
-  return <div className="App"></div>;
-}
+    }
+  });
+  return auxiliaryArr;
+};
 
-export default App;
-///// 3x^2 - 12x + 10 //// -12x
+const processExpression = (expression) => {
+  // FUNCTION FOR PRCESSING ALL OPERATIONS ON EXPRESSION
+  let expArray = splitter(expression);
+  expArray = emptyValueCleaner(expArray);
+  expArray = signDetector(expArray);
+  let coeffArray = coefficientGetter(expArray);
+  coeffArray = expressionSimplifier(coeffArray);
+  return coeffArray;
+};
+
+const addingExpressions = (augend, addend) => {
+  // MAIN FUNCTION, GETTING 2 EXPRESSSIONS AS STRINGS
+  let result = adddingCoef(
+    processExpression(augend),
+    processExpression(addend)
+  );
+  result = removeEmptyAlgExp(result);
+  result = sortResultArray(result);
+  console.log(printResult(result));
+  return printResult(result);
+};
+
+addingExpressions(augend, addend);
+module.exports = {
+  addingExpressions,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,65 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// react-testing-library renders your components to document.body,
+// this adds jest-dom's custom assertions
+import "@testing-library/jest-dom/extend-expect";
+const App = require("./App");
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let exp1 = Array.from(
+  { length: Math.floor(Math.random() * 20) + 1 },
+  () => Math.floor(Math.random() * 100) - 50
+);
+
+let exp2 = Array.from(
+  { length: Math.floor(Math.random() * 10) + 1 },
+  () => Math.floor(Math.random() * 100) - 50
+);
+
+const addingArrayExp = (exp1Arr, exp2Arr) => {
+  let resultArray = [];
+  let shorterArray = exp1Arr.length < exp2Arr.length ? exp1Arr : exp2Arr;
+  let longerArray = exp1Arr.length > exp2Arr.length ? exp1Arr : exp2Arr;
+  shorterArray.forEach((value, index) => {
+    resultArray.push(value + longerArray[index]);
+  });
+  longerArray.splice(0, shorterArray.length);
+  resultArray = [...resultArray, ...longerArray];
+  return resultArray.reverse();
+};
+
+const arrayToStringConverter = (array) => {
+  let stringExp = "";
+  array.forEach((value, index) => {
+    if (value < 0)
+      stringExp = `${stringExp} - ${Math.abs(value)}${
+        array.length - 1 - index !== 0 ? "x" : ""
+      }${array.length - 1 - index !== 0 ? array.length - 1 - index : ""}`;
+    else if (value > 0)
+      stringExp = `${stringExp}${index !== 0 ? " + " : ""}${value}${
+        array.length - 1 - index !== 0 ? "x" : ""
+      }${array.length - 1 - index !== 0 ? array.length - 1 - index : ""}`;
+  });
+  if (stringExp === "") return "0";
+  else return stringExp;
+};
+
+it("adding expressions with integer coefficients generated randomly", () => {
+  expect(
+    App.addingExpressions(
+      arrayToStringConverter(exp1),
+      arrayToStringConverter(exp2)
+    )
+  ).toEqual(
+    arrayToStringConverter(addingArrayExp(exp1.reverse(), exp2.reverse()))
+  );
+});
+
+it("adding expressions with real coefficients ", () => {
+  expect(App.addingExpressions("-25x + 0.3", "55x + 3/10")).toEqual(
+    "30x1 + 0.6"
+  );
+});
+
+it("adding expressions with real exponent ", () => {
+  expect(App.addingExpressions("-25x0.5 + 0.3", "55x + 3/10 + 10x0.5")).toEqual(
+    "55x1 - 15x0.5 + 0.6"
+  );
 });
